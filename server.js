eb3cddc7987966fe8416268c28ed8f5b8c0ba2d2
Mitali <s224582251@deakin.edu.au>
2025-03-20 13:56:14 +1100

    adding comments

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-const express = require("express");
-const app = express();
-const port = 3111;
+const express = require("express");// Import the Express module
+const app = express();// Create an Express application instance
+const port = 3111;// Defining the port number on which the server will run
 
-app.use(express.static("public"));
-
+app.use(express.static("public"));// Middleware 
 app.get("/", (req, res) => {
-    res.send("this is directory");
+    res.send("this is directory");//home page route
 });
-
 
 app.get("/connect", (req, res) => {
-    res.send("this is connect list page");
+    res.send("this is connect list page");// connect page route
 });
-
 
 app.get("/team", (req, res) => {
-    res.send("this is team page");
+    res.send("this is team page");// team page route
 });
+
 app.listen(port, () => {
-    console.log(`Server connected at :${port}`);
+    console.log(`Server connected at :${port}`);//Start the server
 });
 
 
